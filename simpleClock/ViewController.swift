9b6a956cc48e85caaf5874ad8f2ddf7569933be7
victorgubskih <112268920+victorgubskih@users.noreply.github.com>
2022-11-19 14:32:01 +0200

    twelfNineSixThree

--- simpleClock/ViewController.swift
+++ simpleClock/ViewController.swift
@@ -11,20 +11,46 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var simpleClokView: UIView!
     
-
+   
+    @IBOutlet weak var twelfLabel: UILabel!
+    
+    @IBOutlet weak var nineLabel: UILabel!
+    
+    @IBOutlet weak var sixLabel: UILabel!
+    
+    @IBOutlet weak var threeLabel: UILabel!
+    
+    @IBOutlet weak var centerLabel: UILabel!
+    
+   
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        // someCode
+        //centerLabel.center = simpleClokView.center
         simpleClokView.center = view.center
         simpleClokView.layer.borderWidth = 2
         simpleClokView.layer.borderColor = UIColor.black.cgColor
-        
+       
         
         
         simpleClokView.layer.cornerRadius = simpleClokView.frame.size.height / 2
+       
+        twelfLabel.frame.origin.y = 5
+        twelfLabel.center.x = simpleClokView.frame.size.width / 2
         
+        
+        sixLabel.frame.origin.y = simpleClokView.frame.height - sixLabel.frame.height - 5
+        sixLabel.center.x = simpleClokView.frame.size.width / 2
+        
+        nineLabel.frame.origin.x = 0
+        nineLabel.center.y = simpleClokView.frame.size.height / 2
+        
+        threeLabel.frame.origin.x = simpleClokView.frame.width - threeLabel.frame.width
+        threeLabel.center.y = simpleClokView.frame.size.height / 2
     }
-
 
 }
 
